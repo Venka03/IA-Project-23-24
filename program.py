df37cb2e22f6b38bda94e0206185094571db1c30
Venka03 <venka03@knu.ua>
2024-05-09 16:02:34 +0200

    bad aggregation

--- program.py
+++ program.py
@@ -15,6 +15,7 @@ def fuzzyValue(val: int, fuzzyset: FuzzySet) -> int:
 
 def main():
     fuzzySetsDict = readFuzzySetsFile("InputVarSets.txt")
+    fuzzySetsRisks = readFuzzySetsFile("Risks.txt")
     rules = readRulesFile()
     applicationList = readApplicationsFile()  # store for each member it's value and compute their risk
     i = 0
@@ -30,6 +31,10 @@ def main():
         userRules = []
         if i == 1:
             # fuzzySetsDict.printFuzzySetsDict()
+            low = 0
+            medium = 0
+            high = 0
+
             for rule in rules:
                 strength = 1
                 validRule = 1
@@ -47,6 +52,43 @@ def main():
                     rule.strength = strength
                     userRules.append(rule)
                     rule.printRule()
+                    if rule.consequent == "Risk=HighR":
+                        high = max(high, rule.strength)
+                    if rule.consequent == "Risk=MediumR":
+                        medium = max(medium, rule.strength)
+                    if rule.consequent == "Risk=LowR":
+                        low = max(low, rule.strength)
+
+            fuzzySetsRisks["Risk=LowR"].y *= low
+            fuzzySetsRisks["Risk=MediumR"].y *= medium
+            fuzzySetsRisks["Risk=HighR"].y *= high
+
+            """
+                    for key in fuzzySetsRisks:
+                        if rule.consequent == fuzzySetsRisks[key].var + "=" + fuzzySetsRisks[key].label:
+                            fuzzySetsRisks[key].memDegree = rule.strength
+                            fuzzySetsRisks[key].y *= fuzzySetsRisks[key].memDegree
+            """
+
+            #aggreg = np.fmax(fuzzySetsRisks.values[0].y, np.fmax(fuzzySetsRisks.values[1].y, fuzzySetsRisks.values[2].y))
+            aggreg = np.fmax(fuzzySetsRisks["Risk=LowR"].y, fuzzySetsRisks["Risk=MediumR"].y)
+            aggreg = np.fmax(fuzzySetsRisks["Risk=HighR"].y, aggreg)
+            #  aggreg = skf.maxmin_composition(fuzzySetsRisks.values[0].x, fuzzySetsRisks.values[0].y, fuzzySetsRisks.values[0].x, fuzzySetsRisks.values[1].y)
+            #  aggreg = skf.maxmin_composition(fuzzySetsRisks.values[0].x, aggreg, fuzzySetsRisks.values[0].x,  fuzzySetsRisks.values[2].y)
+
+
+            plt.plot(fuzzySetsRisks["Risk=LowR"].x, fuzzySetsRisks["Risk=LowR"].y, label='MF 1')
+            plt.plot(fuzzySetsRisks["Risk=MediumR"].x, fuzzySetsRisks["Risk=MediumR"].y, label='MF 2')
+            plt.plot(fuzzySetsRisks["Risk=HighR"].x, fuzzySetsRisks["Risk=HighR"].y, label='MF 3')
+            plt.plot(fuzzySetsRisks["Risk=LowR"].x, aggreg, label='Aggregated MF', linestyle='--')
+            plt.xlabel('x')
+            plt.ylabel('Membership degree')
+            plt.title('Aggregation using max')
+            plt.legend()
+            plt.show()
+
+
+
         i += 1
 
         #  print(data[0], data[1])
